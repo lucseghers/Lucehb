b4d1b48ad3b3a3d0e4a74028a42d58b4917e6946
lucseghers <168024469+lucseghers@users.noreply.github.com>
2024-06-05 21:46:26 +0200

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+  backend "s3" {
+    bucket         = "lucseghers03"
+    key            = "tfstate/terraform.tfstate"
+    dynamodb_table = "mlopslock"
+    region         = "eu-west-1"
+  }
+}
+provider "aws" {
+  region = "eu-west-1"
+}
